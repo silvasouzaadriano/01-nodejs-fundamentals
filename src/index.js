61c052c1043668e05de2ed8942686a933707294e
adriano.souza <adriano.souza@zpesystems.com>
2022-03-27 20:19:27 -0300

    Using json on get response

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.send('Hello World')
+  return response.json({
+    message: 'Hello World'
+  })
 });
 
 app.listen(3333);
